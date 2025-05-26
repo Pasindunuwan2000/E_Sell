53bc21c0c85b86303a0633d167be8735333701b8
Pasindu Nuwan <154910776+Pasindunuwan2000@users.noreply.github.com>
2025-05-26 16:27:54 +0530

    Initiate The Project

---
+++ Esell/src/test/java/com/esell/EsellApplicationTests.java
@@ -0,0 +1,13 @@
+package com.esell;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EsellApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
